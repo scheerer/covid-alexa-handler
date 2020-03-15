038c44c1694ddc219dcf24aa229abd9d74252053
Russell Scheerer <demo@RScheerer01.local>
2020-03-15 14:25:15 -0400

    Better help messaging

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const HelpIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'You can say hello to me! How can I help?';
+        const speakOutput = 'You can ask me how many cases are there a country! How can I help?';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
